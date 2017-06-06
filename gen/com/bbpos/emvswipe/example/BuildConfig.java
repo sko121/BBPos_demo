d7164297f7a6684960dfdc4542bf37a00526dbf4
charo <charo@thtfit.com>
2017-06-06 15:56:12 +0800

    BBDevice-Android-SDK-3.0.0/BBDeviceAPI-Android-Example

--- gen/com/bbpos/emvswipe/example/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bbpos.emvswipe.example;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = false;
-}
---
+++ src/com/bbpos/bbdevice/example/TLV.java
@@ -0,0 +1,19 @@
+package com.bbpos.bbdevice.example;
+
+import java.util.List;
+
+class TLV implements Comparable<TLV> {
+	
+	public String tag;
+	public String length;
+	public String value;
+	
+	public boolean isNested;
+	public List<TLV> tlvList;
+	
+	@Override
+	public int compareTo(TLV tlv) {
+		return tag.compareToIgnoreCase(tlv.tag);
+	}
+	
+}
